82d62739e2da869385b1fe9b851de3229cf7f11b
Gian Calgeer <gidoca@gmail.com>
2011-02-28 21:54:07 +0100

    Implement sphere intersections

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "camera.h"
 #include "plane.h"
+#include "sphere.h"
 
 #include <iostream>
 
@@ -9,7 +10,8 @@
 int main(int argc, char **argv) {
   QSize resolution(64, 64);
   Camera camera(QVector3D(0, 0, 0), QVector3D(0, 1, 0), QVector3D(0, 0, 1), 1.5, resolution);
-  Plane plane(QVector4D(0, 0, -1, 1));
+  //Plane object(QVector4D(0, 0, -1, 1));
+  Sphere object(QVector3D(0, 4, 0), 0.5);
   QImage image(resolution, QImage::Format_RGB32);
   for(int i = 0; i < image.width(); i++)
   {
@@ -17,7 +19,7 @@ int main(int argc, char **argv) {
     {
       QPoint point = QPoint(i, j);
       Ray ray = camera.getRay(point);
-      if(plane.intersect(ray))
+      if(object.intersect(ray))
       {
 	image.setPixel(point, qRgb(255, 255, 255));
       }
--- sphere.cpp
+++ sphere.cpp
@@ -8,5 +8,10 @@ Sphere::Sphere(QVector3D center, double radius) : center(center), radius(radius)
 //TODO fixme
 bool Sphere::intersect(Ray ray) const
 {
-  return false;
+  QVector3D diff = ray.getOrigin() - center;
+  double radiusSquared = radius * radius;
+  double a = ray.getDirection().lengthSquared();
+  double b = 2 * QVector3D::dotProduct(ray.getDirection(), diff);
+  double c = diff.lengthSquared() - radiusSquared;
+  return b * b - 2 * a * c >= 0;
 }
