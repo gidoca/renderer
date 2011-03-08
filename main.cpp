1dc2229e8dbee1f6e579e2fb3d853840c512ab7c
Gian Calgeer <gidoca@gmail.com>
2011-03-08 23:33:30 +0100

    Use OpenMP

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
   Light * light = getLight();
   Camera camera = getCamera(resolution);
   
+  #pragma omp parallel for
   for(int i = 0; i < image.width(); i++)
   {
     for(int j = 0; j < image.height(); j++)
@@ -24,8 +25,11 @@ int main(int argc, char **argv) {
       Ray ray = camera.getRay(point);
       HitRecord hitRecord = object->intersect(ray);
       Spectrum irradiance = 255 * integrator.integrate(*object, ray, *light);
-      
-      image.setPixel(point, qRgb((int) irradiance.x(), (int) irradiance.y(), (int) irradiance.z()));
+  
+      #pragma omp critical
+      {
+	image.setPixel(point, qRgb((int) irradiance.x(), (int) irradiance.y(), (int) irradiance.z()));
+      }
     }
   }
   
