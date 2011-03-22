cb7f634fed03702c95d278675e52821ab5bd0bc4
Gian Calgeer <gidoca@gmail.com>
2011-03-22 15:12:43 +0100

    Fix (?) transparent material

--- transparentmaterial.cpp
+++ transparentmaterial.cpp
@@ -6,8 +6,9 @@
 
 #include <iostream>
 #include <cmath>
+#include <limits>
 
-TransparentMaterial::TransparentMaterial(double refractionCoefficient): MirrorMaterial(1), refractionCoefficient(1 / refractionCoefficient)
+TransparentMaterial::TransparentMaterial(double refractionCoefficient): MirrorMaterial(1), refractionCoefficient(refractionCoefficient)
 {
 
 }
@@ -26,10 +27,24 @@ Spectrum TransparentMaterial::shade(HitRecord& hit, Light& light, const Intersec
   double angle = acos(cosAngle);
   if(sin(angle) * refractionCoefficient > 1) return mirrorComponent;
   double outAngle = asin(refractionCoefficient * sin(angle));
-  QVector3D refractedDirection = refractionCoefficient * rayDirection + (refractionCoefficient * cosAngle - cos(outAngle)) * hit.getSurfaceNormal();
-  refractedDirection.normalize();
+  QVector3D refractedDirectionFront = refractionCoefficient * rayDirection + (refractionCoefficient * cosAngle - cos(outAngle)) * hit.getSurfaceNormal();
+  QVector3D refractedDirectionBack = 1 / refractionCoefficient * rayDirection + (1 / refractionCoefficient * cosAngle - cos(outAngle)) * hit.getSurfaceNormal();
+  refractedDirectionFront.normalize();
+  refractedDirectionBack.normalize();
   QVector3D newOrigin = hit.getIntersectingPoint();
-  HitRecord newHit = scene.intersect(Ray(newOrigin, refractedDirection));
+  Ray newFrontRay = Ray(newOrigin, refractedDirectionFront);
+  Ray newBackRay = Ray(newOrigin, -refractedDirectionBack);
+  HitRecord newHitFront = scene.intersect(newFrontRay);
+  HitRecord newHitBack = scene.intersect(newBackRay, -EPSILON, -std::numeric_limits< double >::infinity());
+  HitRecord newHit;
+  if(newHitFront.getRayParameter() < newHitBack.getRayParameter())
+  {
+    newHit = newHitFront;
+  }
+  else
+  {
+    newHit = newHitBack;
+  }
   double f = pow(1 - refractionCoefficient, 2) / pow(1 + refractionCoefficient, 2);
   double F = f + (1 - f) * pow(1 - QVector3D::dotProduct(rayDirection, -surfaceNormal), 5);
   return (1 - F) * newHit.getMaterial().shade(newHit, light, scene, depth + 1) + F * mirrorComponent;
