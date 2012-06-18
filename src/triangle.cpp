5cdc61573a8d20649af0d4b29da78be08856282b
Gian Calgeer <gidoca@gmail.com>
2012-06-18 17:44:05 +0200

    Closed formula for triangle intersection

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -9,30 +9,31 @@
 
 HitRecord Triangle::intersect(Ray ray, float from, float to) const
 {
-  if(QVector3D::dotProduct(ray.getDirection(), QVector3D::crossProduct(p1 - p2, p1 - p3)) > 0) return HitRecord();
+//  if(QVector3D::dotProduct(ray.getDirection(), QVector3D::crossProduct(p1 - p2, p1 - p3)) > 0) return HitRecord();
   
-  /*QVector3D edge1 = p1 - p2;
-  QVector3D edge2 = p1 - p3;
-  QVector3D pvec = QVector3D::crossProduct(ray.getDirection().toVector3D(), edge2);
+  QVector3D edge1 = p2 - p1;
+  QVector3D edge2 = p3 - p1;
+  QVector3D pvec = QVector3D::crossProduct(ray.getDirection(), edge2);
   float det = QVector3D::dotProduct(pvec, edge1);
   if(det < EPSILON) return HitRecord();
   
-  QVector3D tvec = ray.getOrigin().toVector3DAffine() - p1;
+  QVector3D tvec = ray.getOrigin() - p1;
   float u = QVector3D::dotProduct(pvec, tvec);
   if(u < 0 || u > det) return HitRecord();
   
   QVector3D qvec = QVector3D::crossProduct(tvec, edge1);
-  float v = QVector3D::dotProduct(ray.getDirection().toVector3D(), qvec);
+  float v = QVector3D::dotProduct(ray.getDirection(), qvec);
   if(v < 0 || u + v > det) return HitRecord();
   
   float t = QVector3D::dotProduct(edge2, qvec);
+  if(t < from || t > to) return HitRecord();
   float inv_det = 1 / det;
   t *= inv_det;
   u *= inv_det;
   v *= inv_det;
-  return HitRecord(t, ray, material, u * n1 + v * n2 + (1 - u - v) * n3);*/
+  return HitRecord(t, ray, material, u * n1 + v * n2 + (1 - u - v) * n3);
   
-  QMatrix4x4 intersectionMatrix;
+  /*QMatrix4x4 intersectionMatrix;
   intersectionMatrix.setColumn(0, p1 - p2);
   intersectionMatrix.setColumn(1, p1 - p3);
   intersectionMatrix.setColumn(2, ray.getDirection());
@@ -50,7 +51,7 @@ HitRecord Triangle::intersect(Ray ray, float from, float to) const
   else
   {
     return HitRecord();
-  }
+  }*/
 }
 
 AxisAlignedBox * Triangle::boundingBox() const
