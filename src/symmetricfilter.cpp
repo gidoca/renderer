f50e22cdbc64023742cf6be6c9defaf6bbcbb612
Gian Calgeer <gidoca@gmail.com>
2012-06-28 10:07:08 +0200

    fix nan

--- src/symmetricfilter.cpp
+++ src/symmetricfilter.cpp
@@ -42,10 +42,11 @@ cv::Mat computeWeights(const Mat& source, const Mat& target, const Mat& var, con
 //            exp(-max(d2 - 2 * pixvar, 0) / (1e-10f + h2 * pixvar), weights);
     //use min(var1, pixvar)
 //    exp(-(d2 - (var + min(shiftedVar, var))) / (h2 * (var + shiftedVar)), temp);
-    exp(-(d2 - (var + shiftedVar)) / (h2 * (var + shiftedVar)), temp);
+    exp(-(d2 - (var + shiftedVar)) / (1e-8f + h2 * (var + shiftedVar)), temp);
     blur(temp, temp2, Size(patchSize, patchSize), Point(-1, -1), BORDER_REFLECT);
     //threshold?
     weights = max(channelMean(temp2), 0);
+    assert(checkRange(weights));
     return weights;
 }
 
@@ -107,6 +108,8 @@ cv::Mat SymmetricFilter::filter(const cv::Mat &image, const cv::Mat &guide, cons
 
             area += weights1 + weights2;
             acc += weights1.mul(data1) + weights2.mul(data2);
+            assert(checkRange(area));
+            assert(checkRange(acc));
         }
     }
 
