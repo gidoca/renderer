fe5c22155cba16eaad9db9434521dec4c22abc9c
Gian Calgeer <gidoca@gmail.com>
2012-09-05 15:12:57 +0200

    Implement basic refraction

--- src/mathhelper.h
+++ src/mathhelper.h
@@ -8,4 +8,19 @@ void var(cv::Mat &mean, cv::Mat &var, std::vector<cv::Mat> in);
 cv::Mat channelMean(const cv::Mat &in);
 cv::Mat extend(const cv::Mat &in, int nchan = 3);
 
+template <typename T> inline constexpr
+int signum(T x, std::false_type is_signed) {
+    return T(0) < x;
+}
+
+template <typename T> inline constexpr
+int signum(T x, std::true_type is_signed) {
+    return (T(0) < x) - (x < T(0));
+}
+
+template <typename T> inline constexpr
+int signum(T x) {
+    return signum(x, std::is_signed<T>());
+}
+
 #endif
